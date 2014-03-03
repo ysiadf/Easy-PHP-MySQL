ed76981cc71a5210e7235b123bae65b5f55fd0b2
Diego <DiegoSanjuan@users.noreply.github.com>
2014-03-03 22:49:04 +0100

    update

--- index.php
+++ index.php
@@ -2,6 +2,8 @@
 
 include 'db_connect.php';  //include the db_connect.php file
 
+print"<H1>Showing the users of the sampledb that has been created in the RDS few seconds ago!</H1>\n"
+
 //Create and set a new connection
 $get = new Connection("db1"); //"db1" is user defined in db_connect.php
 
@@ -9,10 +11,19 @@ $get = new Connection("db1"); //"db1" is user defined in db_connect.php
 $selectusers = $get->query("SELECT * FROM users");
 
 //Do whatever you would like with the query
+echo "Total Number of user records";
+
 echo mysql_num_rows($selectusers);
 
 //Use built in functions to make one line queries.
-echo  $get->fetch($selectusers);  //fetch() fetches the associated rows.  More quick functions coming soon.
+$rowarray = $get->fetch($selectusers);  //fetch() fetches the associated rows.
 
+print "<tr>\n";  
+foreach ($rowarray as $row) {
+    foreach ($row as $col) {
+        print "\t<td>$col</td>\n";
+    }
+    print "</tr>\n";
+}  
 
 ?>
