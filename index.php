f399c5f1c2e37a730f8612cd30ff3c12fe7d3c99
Diego <DiegoSanjuan@users.noreply.github.com>
2014-03-03 20:52:16 +0100

    Binding as variable

---
+++ index.php
@@ -0,0 +1,33 @@
+<?php
+
+include 'db_connect.php';  //include the db_connect.php file
+
+print "<HTML><BODY><H1>Showing the users of the sampledb that has been created in the RDS few seconds ago!</H1>\n";
+
+//Create and set a new connection
+$get = new Connection("db1"); //"db1" is user defined in db_connect.php
+
+//Create a query: $var->query("SQL_STATEMENT"); $var is the variable you created a connection with.
+$selectusers = $get->query("SELECT * FROM users");
+
+//Do whatever you would like with the query
+print "Total Number of user records: ";
+
+print mysql_num_rows($selectusers);
+
+print "\n\r";
+
+//Use built in functions to make one line queries.
+$rowarray = $get->fetch($selectusers);  //fetch() fetches the associated rows.
+
+print "<table>\n\r";  
+foreach ($rowarray as $row) {
+    print "<tr>\n\r";  
+    foreach ($row as $col) {
+        print "\t<td>$col</td>\n\r";
+    }
+    print "</tr>\n\r";
+}
+print "</table></BODY></HTML>";
+
+?>
