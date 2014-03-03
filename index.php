1404fe7767a830525b90175ae147acdcb0d80eac
Diego <DiegoSanjuan@users.noreply.github.com>
2014-03-03 23:03:02 +0100

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 
 include 'db_connect.php';  //include the db_connect.php file
 
-print "<H1>Showing the users of the sampledb that has been created in the RDS few seconds ago!</H1>\n";
+print "<HTML><BODY><H1>Showing the users of the sampledb that has been created in the RDS few seconds ago!</H1>\n";
 
 //Create and set a new connection
 $get = new Connection("db1"); //"db1" is user defined in db_connect.php
@@ -11,19 +11,21 @@ $get = new Connection("db1"); //"db1" is user defined in db_connect.php
 $selectusers = $get->query("SELECT * FROM users");
 
 //Do whatever you would like with the query
-print "Total Number of user records";
+print "Total Number of user records: ";
 
 print mysql_num_rows($selectusers);
+
+print "\n\r";
 
 //Use built in functions to make one line queries.
 $rowarray = $get->fetch($selectusers);  //fetch() fetches the associated rows.
 
-print "<tr>\n";  
+print "<table><tr>\n\r";  
 foreach ($rowarray as $row) {
     foreach ($row as $col) {
-        print "\t<td>$col</td>\n";
+        print "\t<td>$col</td>\n\r";
     }
-    print "</tr>\n";
+    print "</tr>\n\r</table></BODY></HTML>";
 }  
 
 ?>
